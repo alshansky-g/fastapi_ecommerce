e8116440e79845b8ac1ff1a43d10e11389026657
alshansky-g <rgcs.rgcs@gmail.com>
2025-09-17 23:39:07 +0300

    feat: add post category endpoint

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select, update
+from sqlalchemy.orm import Session
+
+from app.db_depends import get_db
+from app.models.categories import Category as CategoryModel
+from app.schemas import Category as CategorySchema
+from app.schemas import CategoryCreate
 
 router = APIRouter(
     prefix="/categories", tags=["categories"],
@@ -11,10 +20,23 @@ async def get_all_categories():
     return {"message": 'заглушка'}
 
 
-@router.post("/")
-async def create_category():
+@router.post("/", response_model=CategoryModel,
+             status_code=status.HTTP_201_CREATED)
+async def create_category(category: CategoryCreate,
+                          db: Annotated[Session, Depends(get_db)]):
     """Создает новую категорию."""
-    return {"message": "заглушка"}
+    if category.parent_id is not None:
+        stmt = select(CategoryModel).where(
+            CategoryModel.id == category.parent_id, CategoryModel.is_active)
+        parent = db.scalars(stmt).first()
+        if parent is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail="Parent category not found")
+    db_category = CategoryModel(**category.model_dump())
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+    return db_category
 
 
 @router.put("/{category_id}")
