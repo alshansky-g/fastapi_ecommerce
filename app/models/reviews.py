424454d0bbac6481e73d8a7f5f5d061d990c8db6
alshansky-g <rgcs.rgcs@gmail.com>
2025-09-30 12:41:24 +0300

    feat: add review model

---
+++ app/models/reviews.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, Text, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database import Base
+
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
+    comment: Mapped[str] = mapped_column(Text)
+    comment_date: Mapped[datetime] = mapped_column(default=func.now)
+    grade: Mapped[int] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True)
